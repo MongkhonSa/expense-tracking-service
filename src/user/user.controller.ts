147144dae1daf7f03db4eeb6da8741ce18ba7fac
Toktak Mongkhon <mongkhon@rentspree.com>
2022-08-25 00:59:42 +0700

    remove authen required for create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
-  @UseGuards(JwtAuthGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
